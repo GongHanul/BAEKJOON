4fbad62a652deab9c3f31d0e167cb2cefcc5af64
GongHanul <70622941+GongHanul@users.noreply.github.com>
2022-08-27 22:08:24 +0900

    [Gold IV] Title: 연구소, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,73 +1,85 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
-#include <vector>
 using namespace std;
+int N, M;
+int map[8][8];
+int visited[8][8];
+int blankCnt = 0;
 struct Node {
 	int row, col;
 };
-
-int MAP[10][10];
-int N, M;
+vector<Node> wall;
+vector<Node> virus;
 vector<Node> blank;
-queue<Node> virus;
 
-int dr[] = { 0, 0, 1, -1 };
-int dc[] = { 1, -1, 0, 0 };
-
-int bfs() {
-	int blankCnt = 0;
-	int visited[10][10] = { 0, };
-	queue<Node> q=virus;
-	while (!q.empty()) {
-		Node now = q.front();
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int n_row = now.row + dr[i];
-			int n_col = now.col + dc[i];
-			if (MAP[n_row][n_col] != 0) continue;
-			if (visited[n_row][n_col]) continue;
-			visited[n_row][n_col] = 1;
-			blankCnt++;
-			q.push({ n_row, n_col });
-		}
-	}
-	return blank.size() - 3 - blankCnt;
+void setWall(Node now, int check) {
+		map[now.row][now.col] = check;
 }
 
-void SetWall(Node coord, int stat) {
-	MAP[coord.row][coord.col] = stat;
+void dfs(Node now) {
+	int dr[] = { 0, 0, 1, -1 };
+	int dc[] = { 1, -1, 0, 0 };
+	for (int i = 0; i < 4; i++) {
+		int nr = now.row + dr[i];
+		int nc = now.col + dc[i];
+		if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+		if (map[nr][nc] != 0) continue;
+		if (visited[nr][nc] != 0) continue;
+		visited[nr][nc] = 1;
+		blankCnt++;
+		dfs({ nr, nc });
+	}
 }
 
 int solution() {
-	int maxVal = 0;
+	int maxval = 0;
 	for (int i = 0; i < blank.size(); i++) {
 		for (int j = i + 1; j < blank.size(); j++) {
 			for (int k = j + 1; k < blank.size(); k++) {
-				SetWall(blank[i], 1);
-				SetWall(blank[j], 1);
-				SetWall(blank[k], 1);
-				maxVal = max(maxVal, bfs());
-				SetWall(blank[i], 0);
-				SetWall(blank[j], 0);
-				SetWall(blank[k], 0);
+				setWall(blank[i], 1);
+				setWall(blank[j], 1);
+				setWall(blank[k], 1);
+				
+				for (int row = 0; row < 8; row++)
+				{
+					for (int col = 0; col < 8; col++)
+					{
+						visited[row][col] = 0;
+					}
+				}
+				blankCnt = 0;
+				for (int i = 0; i < virus.size(); i++)
+				{
+					dfs(virus[i]);
+				}
+				maxval = max(maxval, (int)blank.size() - 3 - blankCnt);
+
+				setWall(blank[i], 0);
+				setWall(blank[j], 0);
+				setWall(blank[k], 0);
+
 			}
 		}
 	}
-	return maxVal;
+	return maxval;
 }
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> N >> M;
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			MAP[i][j] = 1;
-		}
-	}
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			cin >> MAP[i][j];
-			if (MAP[i][j] == 0) blank.push_back({ i, j });
-			if (MAP[i][j] == 2) virus.push({ i, j });
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 2) {
+				virus.push_back({ i, j });
+			}
+			else if (map[i][j] == 0) {
+				blank.push_back({ i, j });
+			}
 		}
 	}
 
