6b493acc0882ac51d7437269dba5824d9125ff04
GongHanul <70622941+GongHanul@users.noreply.github.com>
2022-11-20 20:52:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 539 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.cpp
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
 
 int N, ans;
 int map[16][16];
-int dat[16] = { 0, };
+int check[16];
 
-void dfs(int index, int cnt) {
-	if (cnt == N / 2) {
-		vector<int> vA, vB;
-		int fA = 0, fB = 0;
+void simul(int idx, int cnt) {
+	if (cnt == N/2) {
+		vector<int> f1, f2;
 		for (int i = 0; i < N; i++) {
-			if (dat[i] == 1) vA.push_back(i);
-			else vB.push_back(i);
+			if (check[i] == 1) f1.push_back(i);
+			else f2.push_back(i);
 		}
-		for (int i = 0; i < vA.size() - 1; i++) {
-			for (int j = i+1; j < vA.size(); j++) {
-				fA += map[vA[i]][vA[j]] + map[vA[j]][vA[i]];
-				fB += map[vB[i]][vB[j]] + map[vB[j]][vB[i]];
+		int f1_food = 0, f2_food = 0;
+		for (int i = 0; i < f1.size() - 1; i++) {
+			for (int j = i + 1; j < f1.size(); j++) {
+				f1_food += map[f1[i]][f1[j]] + map[f1[j]][f1[i]];
+				f2_food += map[f2[i]][f2[j]] + map[f2[j]][f2[i]];
 			}
 		}
-		ans = min(ans, abs(fA - fB));
+		ans = min(ans, abs(f1_food - f2_food));
+		return;
 	}
-	for (int i = index; i < N; i++) {
-		if (dat[i] == 0) {
-			dat[i] = 1;
-			dfs(i, cnt + 1);
-			dat[i] = 0;
-		}
+
+	for (int i = idx; i < N; i++) {
+		if (check[i] != 0) continue;
+		check[i] = 1;
+		simul(i, cnt+1);
+		check[i] = 0;
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int T;
-	cin >> T;
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int T; cin >> T;
 	for (int t = 1; t <= T; t++) {
 		cin >> N;
 		for (int i = 0; i < N; i++) {
@@ -46,8 +43,8 @@ int main() {
 			}
 		}
 		ans = 987654321;
-		dfs(0, 0);
-        cout<<"#"<<t<<" "<<ans<<"\n";
+		simul(0, 0);
+		cout << "#" << t << " " << ans << "\n";
 	}
 	return 0;
 }
