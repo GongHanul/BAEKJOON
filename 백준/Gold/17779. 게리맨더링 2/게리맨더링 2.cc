9f6a91f34c44dcbeab765a3862e2a51f9c809622
GongHanul <70622941+GongHanul@users.noreply.github.com>
2023-06-08 23:22:14 +0900

    [Gold III] Title: 게리맨더링 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.cc
+++ 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.cc
@@ -4,53 +4,48 @@ using namespace std;
 
 int N, ans;
 int map[25][25];
+int Sect[5];
 
 int sect(int row, int col, int d1, int d2) {
-	int visited[25][25] = { 0, };
-	int maxx = -1, minn = 213456789;
 	// sect 1
 	int sum = 0, idx = col;
 	for (int i = 1; i < row + d1; i++) {
 		for (int j = 1; j <= idx; j++) {
 			sum += map[i][j];
-			visited[i][j] = 1;
 		}
 		if(i+1 >= row) idx -= 1;
 	}
-	maxx = max(maxx, sum), minn = min(minn, sum);
+	Sect[0] = sum;
 
 	// sect 2
 	sum = 0, idx = col + 1;
 	for (int i = 1; i <= row + d2; i++) {
 		for (int j = idx; j <= N; j++) {
 			sum += map[i][j];
-			visited[i][j] = 2;
 		}
 		if (i+1 > row) idx += 1;
 	}
-	maxx = max(maxx, sum), minn = min(minn, sum);
+	Sect[1] = sum;
 
 	// sect 3
 	sum = 0, idx = col - d1;
 	for (int i = row + d1; i <= N; i++) {
 		for (int j = 1; j < idx; j++) {
 			sum += map[i][j];
-			visited[i][j] = 3;
 		}
 		if (i < row + d1 + d2) idx += 1;
 	}
-	maxx = max(maxx, sum), minn = min(minn, sum);
+	Sect[2] = sum;
 
 	// sect 4
 	sum = 0, idx = col + d2;
 	for (int i = row + d2 + 1; i <= N; i++) {
 		for (int j = idx; j <= N; j++) {
 			sum += map[i][j];
-			visited[i][j] = 4;
 		}
 		if (i <= row + d1 + d2) idx -= 1;
 	}
-	maxx = max(maxx, sum), minn = min(minn, sum);
+	Sect[3] = sum;
 
 	// sect 5
 	sum = 0;
@@ -58,33 +53,28 @@ int sect(int row, int col, int d1, int d2) {
 	for (int i = row; i <= row + d1 + d2; i++) {
 		for (int j = idx1; j < idx2; j++) {
 			sum += map[i][j];
-			visited[i][j] = 5;
 		}
 		if (i < row + d1) idx1 -= 1;
 		if (i < row + d2) idx2 += 1;
 		if (i >= row + d1) idx1 += 1;
 		if (i >= row + d2) idx2 -= 1;
 	}
-	maxx = max(maxx, sum), minn = min(minn, sum);
+	Sect[4] = sum;
+
+	int maxx = -1, minn = 213456789;
+	for (int i = 0; i < 5; i++) {
+		maxx = max(maxx, Sect[i]);
+		minn = min(minn, Sect[i]);
+	}
 
 	return maxx - minn;
 }
 
 void simul(int row, int col) {
-	int visited[25][25] = {0, };
 	for (int d1 = 1; d1 < col; d1++) {
 		for (int d2 = 1; d2 <= N - col; d2++) {
 			if (row + d1 + d2 > N) continue;
-			visited[row][col] = 1;
-			visited[row + d1][col - d1] = 1;
-			visited[row + d2][col + d2] = 1;
-			visited[row + d1 + d2][col - d1 + d2] = 1;
 			ans = min(ans, sect(row, col, d1, d2));
-
-			visited[row][col] = 0;
-			visited[row + d1][col - d1] = 0;
-			visited[row + d2][col + d2] = 0;
-			visited[row + d1 + d2][col - d1 + d2] = 0;
 		}
 	}
 }
