2e93263e161a7c33f55c58de1a9b1e56354c17e4
GongHanul <70622941+GongHanul@users.noreply.github.com>
2024-03-22 22:37:54 +0900

    [Silver V] Title: 교수님 저는 취업할래요, Time: 60 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/18221. 교수님 저는 취업할래요/교수님 저는 취업할래요.cc
+++ 백준/Silver/18221. 교수님 저는 취업할래요/교수님 저는 취업할래요.cc
@@ -6,7 +6,7 @@ int map[1001][1001] = { 0, };
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
-	int ra=-1, ca=-1, rb=-1, cb=-1;
+	int ra=-1, ca=-1, rb=-1, cb=-1, student=0;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> map[i][j];
@@ -28,23 +28,6 @@ int main() {
 	int max_r = max(ra, rb), min_r = min(ra, rb);
 	int max_c = max(ca, cb), min_c = min(ca, cb);
 
-	int student = 0;
-	if (ra == rb) {
-		for (int i = min_c; i <= max_c; i++) {
-			if (map[ra][i] == 1) student += 1;
-		}
-	}
-	else if (ca == cb) {
-		for (int i = min_r; i <= max_r; i++) {
-			if (map[i][ca] == 1) student += 1;
-		}
-	}
-	if (student >= 3) {
-		cout << 1;
-		return 0;
-	}
-
-	student = 0;
 	for (int i = min_r; i <= max_r; i++) {
 		for (int j = min_c; j <= max_c; j++) {
 			if (map[i][j] == 1) student += 1;
