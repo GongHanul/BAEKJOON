dd0ac511fd09ad191aef3d9c24b809c290d877ec
GongHanul <70622941+GongHanul@users.noreply.github.com>
2024-03-25 23:05:51 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -2,22 +2,9 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int N, cnt = 0; cin >> N;
+	int N; cin >> N;
 
-	while (1) {
-		if (N > 3) {
-			N -= 3;
-		}
-		else {
-			N -= 1;
-		}
-
-		cnt += 1;
-		if (N == 0) break;
-	}
-
-	if (cnt % 2 == 0) cout << "CY";
+	if (N % 2 == 0) cout << "CY";
 	else cout << "SK";
 
 
